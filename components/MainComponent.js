02235c14698db55706862c8f155fbb811e5b4ebe
Sudarsan S <srisudarsan2000@gmail.com>
2020-09-06 00:19:23 +0530

    Navigation part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import Dishdetail from './DishdetailComponent';
+import { View, Platform } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { ListItem } from 'react-native-elements';
+import Constants from 'expo-constants';
+const barHeight = Constants.statusBarHeight;
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  Dishdetail: {screen: Dishdetail}
+}, {
+  initialRouteName: 'Menu',
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#512DA8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: '#fff'
+    }
+  }
+})
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
   render() {
- 
     return (
-        <Menu dishes={this.state.dishes} />
+    <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : barHeight }}>
+        <MenuNavigator /> 
+    </View>
     );
   }
 }
   
-export default Main;
+export default createAppContainer(MenuNavigator);
